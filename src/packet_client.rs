8a00f66911fda39e5faeeb4904b2533f9fc056a5
Lucas Witte <L.C.Witte@student.tudelft.nl>
2024-05-16 12:21:49 +0200

    Fix pipeline

--- src/packet_client.rs
+++ src/packet_client.rs
@@ -11,9 +11,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Hardcoded for now. The packet data needs to be the intercepted packet data.
     let packet_data: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let request = tonic::Request::new(Packet {
-        data: packet_data,
-    });
+    let request = tonic::Request::new(Packet { data: packet_data });
 
     let response = client.send_packet(request).await?;
     println!("Response: {:?}", response);
