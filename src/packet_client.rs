32f5622c2e64c4fd897a3ec533d0298d5dd90435
Yuraj Mangalgi <Y.Mangalgi@student.tudelft.nl>
2024-05-16 15:47:28 +0200

    On the rust side changed the proto file and updated the sending of the packet such that it doens't only recieve an acknowledgement but now acquires the port and the data that is send

--- src/packet_client.rs
+++ src/packet_client.rs
@@ -7,14 +7,21 @@ pub mod proto {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // make a client
     let mut client = PacketServiceClient::connect("http://[::1]:50051").await?;
 
-    // Hardcoded for now. The packet data needs to be the intercepted packet data.
+    //Hardcoded for now. The packet data needs to be the intercepted packet data.
     let packet_data: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let request = tonic::Request::new(Packet { data: packet_data });
+    let packet_port: u32 = 2;
+    //  Hardcoded port
 
-    let response = client.send_packet(request).await?;
-    println!("Response: {:?}", response);
+    let request = tonic::Request::new(Packet {
+        data: packet_data,
+        port: packet_port,
+    }); // add port data
+
+    let response = client.send_packet(request).await?; // we send to controller and are waiting for the response
+    println!("Response: {:?}", response); // seperate by bytes, port, message
 
     Ok(())
 }
