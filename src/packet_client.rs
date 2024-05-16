90a80e00f4cfca72b1df83a49b9c9c9402beb96d
Lucas Witte <L.C.Witte@student.tudelft.nl>
2024-05-16 11:56:01 +0200

    Basic implementation gRPC to send bytes from rust to python and an acknoledgement string back to rust

---
+++ src/packet_client.rs
@@ -0,0 +1,22 @@
+use proto::packet_service_client::PacketServiceClient;
+use proto::Packet;
+
+pub mod proto {
+    tonic::include_proto!("packet");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = PacketServiceClient::connect("http://[::1]:50051").await?;
+
+    // Hardcoded for now. The packet data needs to be the intercepted packet data.
+    let packet_data: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let request = tonic::Request::new(Packet {
+        data: packet_data,
+    });
+
+    let response = client.send_packet(request).await?;
+    println!("Response: {:?}", response);
+
+    Ok(())
+}
