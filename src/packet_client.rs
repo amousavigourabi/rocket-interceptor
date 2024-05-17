14e315038485a7455ca74e9f950665249294f89e
Yuraj Mangalgi <Y.Mangalgi@student.tudelft.nl>
2024-05-17 11:59:45 +0200

    Made checks to make the method return errors in case the wrong request data is sent so that the method doesn't always return Ok status

--- src/packet_client.rs
+++ src/packet_client.rs
@@ -9,10 +9,21 @@ async fn send_packet(request: tonic::Request<Packet>) -> Result<(), Box<dyn std:
     if request.get_ref().data.is_empty() {
         return Err("Packet data is empty".into());
     }
+
+    let port = match request.get_ref().port {
+        u32::MAX => return Err("Port not set properly".into()),
+        port => port,
+    };
+
     let mut client = PacketServiceClient::connect("http://[::1]:50051").await?;
 
-    let response = client.send_packet(request).await?; // we send to controller and are waiting for the response
-    println!("Response: {:?}", response); // seperate by bytes, port, message
+    let response = client.send_packet(request).await?.into_inner(); // we send to controller and are waiting for the response
+    println!("Response: {:?}", response);
+    // seperate by bytes, port, message
+
+    if response.action < 0 {
+        return Err("Received a negative action in the response".into());
+    }
 
     Ok(())
 }
@@ -50,10 +61,27 @@ mod tests {
         assert!(result.await.is_ok());
     }
     #[tokio::test]
-    async fn assert_result_err() {
+    async fn assert_empty_bytes() {
         // Prepare a request with invalid data
         let packet_data: Vec<u8> = vec![]; // Empty data
         let packet_port: u32 = 2;
+        let request = tonic::Request::new(Packet {
+            data: packet_data,
+            port: packet_port,
+        });
+
+        // Call the async function and obtain the result
+        let result = send_packet(request).await;
+
+        // Assert that the result is not Ok (i.e., Err)
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn assert_empty_port() {
+        // Prepare a request with invalid data
+        let packet_data: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Empty data
+        let packet_port: u32 = u32::MAX;
         let request = tonic::Request::new(Packet {
             data: packet_data,
             port: packet_port,
